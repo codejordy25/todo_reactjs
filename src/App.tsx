f092729700a7f4a1411022cadd9e63a99d63f28a
codejordy25 <jordytsimba11@gmail.com>
2025-09-03 06:52:51 +0200

    create new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
+import { useState } from "react";
+
+type Priority = "Urgente" | "Moyenne" | "Forte";
+
+type Todo = {
+  id: number;
+  title: string;
+  priority: Priority;
+};
+
 function App() {
+  const [input, setInput] = useState<string>("");
+  const [priority, setPriority] = useState<Priority>("Moyenne");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function handleAddTodo() {
+    if (input.trim() === "") {
+      return;
+    }
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: input.trim(),
+      priority: priority,
+    };
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+    // setTodos([...todos, newTodo]);
+    setInput("");
+    setPriority("Moyenne");
+  }
+
   return (
-    <div>
-      <h1>Hello World</h1> <button className="btn btn-accent">Click Me</button>
+    <div className="flex justify-center">
+      {" "}
+      <div className="w-2/3 flex flex-col gap-4 my-15 p-5 bg-base-300 rounded-2xl">
+        <div className="flex gap-4">
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Ajouter une tache"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <select
+            className="select w-full"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value as Priority)}
+            title="Sélectionnez la priorité"
+          >
+            <option value="Urgente">Urgente</option>
+            <option value="Moyenne">Moyenne</option>
+            <option value="Basse">Basse</option>
+          </select>
+          <button className="btn btn-primary" onClick={handleAddTodo}>
+            Ajouter
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
